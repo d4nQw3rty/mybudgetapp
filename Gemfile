c4c48511f06be50e4d84ece604d4d29556a8aeb3
d4nQw3rty <danlinmoy@gmail.com>
2023-02-16 10:19:07 -0400

    add devise

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.1.3"
 gem 'rubocop', '>= 1.0', '< 2.0'
 gem 'dotenv-rails'
 gem 'rspec-rails', group: %i[development test]
+gem 'devise'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
--- db/migrate/20230216140747_create_payments.rb
+++ db/migrate/20230216140747_create_payments.rb
@@ -5,8 +5,10 @@ class CreatePayments < ActiveRecord::Migration[7.0]
       t.float :amount, default: 0.0
       t.references :user, index: true, foreign_key: true
       t.references :group, index: true, foreign_key: true
-      
+
       t.timestamps
     end
   end
 end
+
+# https://www.hackerrank.com/challenges/ruby-hash-method-each/problem?isFullScreen=true
