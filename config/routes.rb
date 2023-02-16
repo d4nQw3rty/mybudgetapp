9c8587abdd8dfc8a60432f56f697b73377402ec5
d4nQw3rty <danlinmoy@gmail.com>
2023-02-16 11:10:35 -0400

    Set up home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'groups#index'
+  root to: 'home#index'
   resources :payments
   resources :groups
   resources :users
+  resources :home
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
