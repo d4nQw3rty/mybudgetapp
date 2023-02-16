27f82c650b33127065636ec4aa97fbd03c105fab
d4nQw3rty <danlinmoy@gmail.com>
2023-02-16 13:27:50 -0400

    Fix linters

--- Gemfile
+++ Gemfile
@@ -62,7 +62,6 @@ group :development do
   gem 'letter_opener'
   gem 'web-console'
 
-
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -76,7 +76,6 @@ RSpec.describe '/payments', type: :request do
         end.to change(Payment, :count).by(0)
       end
 
-
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
         post payments_url, params: { payment: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
