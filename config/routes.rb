f9551eb5b8ce30ce98370fe4fd87ecbc9d72d300
d4nQw3rty <danlinmoy@gmail.com>
2023-02-17 09:40:53 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  
+  root to: "home#index"
   devise_for :users
-
-  root ("groups#index")
   resources :payments
   resources :groups
   resources :users
+  resources :home
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
