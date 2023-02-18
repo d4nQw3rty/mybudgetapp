b4e01df8698125a02176a10e77cb326d70a9c3bc
d4nQw3rty <danlinmoy@gmail.com>
2023-02-18 16:38:45 -0400

    update app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,4 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
-
-  def after_sign_up_path_for(_resource_or_scope)
-    root_path
-  end
 end
