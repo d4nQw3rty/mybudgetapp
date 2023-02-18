102be93be88e6127b57069acf88dcee457213c02
d4nQw3rty <danlinmoy@gmail.com>
2023-02-18 15:23:59 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
-
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -23,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
   
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
