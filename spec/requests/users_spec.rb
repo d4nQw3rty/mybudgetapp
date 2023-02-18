8d3b486254c0173c3181920a396a47ef1fcb07cd
d4nQw3rty <danlinmoy@gmail.com>
2023-02-18 12:21:26 -0400

    Create users_spec.rb

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  before(:each) do
+    @user = User.new(name: 'cesar', email: 'cesar@4a6z.com', password: '123456', password_confirmation: '123456')
+    @user.skip_confirmation!
+    @user.confirm
+    @user.save
+    sign_in @user
+  end
+
+  describe 'group#index' do
+    it 'works! (now write some real specs)' do
+      get groups_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get groups_path
+      expect(response).to render_template('index')
+    end
+
+    it 'does not render a different template' do
+      get groups_path
+      expect(response).to_not render_template('new')
+    end 
+  end
+end
